7a73108bc1de871e75675ec4af4806003a91d0a0
Dan <dan@Dans-MacBook.fios-router.home>
2016-05-14 17:23:35 -0400

    init commit with static family tree

---
+++ gulpfile.js
@@ -0,0 +1,38 @@
+var gulp = require('gulp'),
+    connect = require('gulp-connect'),
+    traceur =  require('gulp-traceur'),
+    sass = require('gulp-ruby-sass');
+
+
+gulp.task('connect', function(){
+    connect.server({
+        livereload: true,
+        port: 8005
+    });
+});
+
+
+gulp.task('reload', function(){
+    gulp.src('./dist/**/*.*')
+        .pipe(connect.reload());
+});
+
+gulp.task('sass', function(){
+        sass('./sass/*.scss')
+        .pipe(gulp.dest('dist/css'));
+});
+
+gulp.task('traceur', function(){
+    gulp.src('./scripts/*.js')
+    .pipe(traceur())
+    .pipe(gulp.dest('dist/scripts'));
+});
+
+gulp.task('watch', function(){
+    gulp.watch(['./sass/*.scss'], ['sass']);
+    gulp.watch(['./scripts/*.js'], ['traceur']);
+    gulp.watch(['./**/*.*'], ['reload']);
+
+});
+
+gulp.task('default', ['connect', 'traceur', 'watch', 'sass']);
