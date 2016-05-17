773ffd766187e5417c9fe18b4790d28b941b8d82
Wensheng Chen <wensheng.chen@nytweb.com>
2016-05-17 16:38:32 -0400

    added add relative form refactored code for easier maintainability and readability

--- scripts/global.js
+++ scripts/global.js
@@ -1,15 +1,3 @@
-$(function() {
-  $('#node-modal').on('show.bs.modal', function (e) {
-    console.log('a wild popup has appear!');
-    console.log(e);
-    $('#node-options').removeClass('hidden');
-    $('#add-node').removeClass('hidden').addClass('hidden');
-  });
-
-  $('#node-modal .add-relative').on('click', function(e) {
-    console.log('click add relative');
-    console.log(e);
-    $('#node-options').toggleClass('hidden');
-    $('#add-node').toggleClass('hidden');
-  });
+$(() => {
+  var nodeModal = new NodeModal('#node-modal');
 });
